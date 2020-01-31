c950e1966b4f6467ce50a83a769b38964e816023
yangyi <1028304016@qq.com>
2020-01-31 16:28:29 +0800

    配置

--- common/config.js
+++ common/config.js
@@ -5,8 +5,7 @@ export default {
 	 */
     //baseUrl: 'http://192.168.1.21:8089/poetry',公司
 	//baseUrl: 'http://192.168.0.101:8089/poetry',家
-	//baseUrl: 'http://192.168.31.207:8089/poetry',//昊邦
-	baseUrl: 'http://192.168.0.105:8088/poetry',//老家
+	baseUrl: 'http://172.20.10.2:8088/poetry',
 	
 	/**
 	 * appid
